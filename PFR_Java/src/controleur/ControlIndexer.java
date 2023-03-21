29fe5c6fbd77d98eaefea116acbd01b57b6f5109
valembrouille <ambrerouzade7@gmail.com>
2023-03-21 23:34:36 +0100

    indexationJava

--- PFR_Java/src/controleur/ControlIndexer.java
+++ PFR_Java/src/controleur/ControlIndexer.java
@@ -1,10 +1,26 @@
-package controleur;
+package Indexation;
 
 public class ControlIndexer {
-	public ControlIndexer() {
+
+	public void indexerTout(String chemin) {
+		indexationTexte(chemin);
+		indexationImage(chemin);
+		indexationSon(chemin);
+	}
+
+	public void indexationTexte(String chemin) {
+		// appel indexationTexte(chemin) de C
 		
 	}
 
-	
+	public void indexationImage(String chemin) {
+		// appel indexationImage(chemin) de C
+		
+	}
+
+	public void indexationSon(String chemin) {
+		// appel indexationSon(chemin) de C
+		
+	}
 	
 }
--- PFR_Java/src/vueconsole/BoundaryIndexer.java
+++ PFR_Java/src/vueconsole/BoundaryIndexer.java
@@ -1,17 +1,43 @@
-package vueconsole;
-
-import controleur.ControlIndexer;
+package Indexation;
 
 public class BoundaryIndexer {
-        ControlIndexer controlIndexer ; 
-        
-        public BoundaryIndexer(ControlIndexer controlIndexer ) {
-        	this.controlIndexer = controlIndexer ; 
-        }
-        
-		public int indexer() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	private ControlIndexer controlIndexer;
+	
+	public BoundaryIndexer(ControlIndexer controlIndexer) {
+		this.controlIndexer=controlIndexer;
 	}
-
+	
+	public void indexer() {
+		int choix=0;
+		System.out.println("Indexation: Veuillez effectuer un choix");
+		while ((choix!=1) || (choix!=2) || (choix!=3) || (choix!=4)) {
+			System.out.println("1. Indexer tous les fichiers");
+			System.out.println("2. Indexer que les textes");
+			System.out.println("3. Indexer que les images");
+			System.out.println("4. Indexer que les audios");
+			choix=Clavier.entrerClavierInt();
+			if ((choix!=1) || (choix!=2) || (choix!=3) || (choix!=4)) {
+				System.out.println("Votre choix doit être compris entre 1 et 4, faites un choix");
+			}
+		}
+		
+		System.out.println("Saisir le chemin du dossier que vous voulez indexer");
+		String chemin=Clavier.entrerClavierString();
+		switch(choix) {
+			case 1:
+				controlIndexer.indexationTout(chemin);
+				break;
+			case 2:
+				controlIndexer.indexationTexte(chemin);
+				break;
+			case 3:
+				controlIndexer.indexationImage(chemin);
+				break;
+			default:
+				controlIndexer.indexationSon(chemin);
+		}
+		System.out.println("Indexation Réussie");
+	}
+	
 }
