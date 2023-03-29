e08e9f4e0050eb9ec28cf48555118af07e7b4c6f
Axelado <67108930+Axelado@users.noreply.github.com>
2023-03-29 22:37:27 +0200

    Add files via upload

--- PFR_Java/src/vueconsole/BoundaryRechercherTexte.java
+++ PFR_Java/src/vueconsole/BoundaryRechercherTexte.java
@@ -8,7 +8,7 @@ import modele.Fichier;
 import modele.TypeRechercheTexte;
 
 public class BoundaryRechercherTexte {
-	ControlRechercherTexte controlRechercher;
+	private ControlRechercherTexte controlRechercher;
 
 	public BoundaryRechercherTexte(ControlRechercherTexte controlRechercher) {
 		super();
@@ -36,33 +36,10 @@ public class BoundaryRechercherTexte {
 		switch(choixTypeTexte) {
 	
 		case 1:
-			int choixTypeMotClef;
-			do {
-				System.out.println("Voulez-vous faire une recherche par mot clefs simple ou complexe ?");
-				System.out.println("Tapez '1' pour une recherche par mot clef simple");
-				System.out.println("Tapez '2' pour une recherche par mot clef complexe");
-				
-				choixTypeMotClef = Clavier.entrerClavierInt();
-				
-				if(!(choixTypeMotClef == 1 || choixTypeMotClef == 2)) {
-					System.out.println("Veuillez entrer 1 ou 2");
-				}
-			}while (!(choixTypeMotClef == 1 || choixTypeMotClef == 2));
-
-			switch(choixTypeMotClef) {
-		
-			case 1:
-				System.out.println("Entrez le mot clef");
-				String motClef = Clavier.entrerClavierString();
-				resultats = controlRechercher.rechercherTexteMotClef(TypeRechercheTexte.SIMPLE, motClef);
-				break;
+			System.out.println("Entrez les mot clefs, en les séparant par une virgules ','");
+			String motsClefs = Clavier.entrerClavierString();
+			resultats = controlRechercher.rechercherTexteMotClef(TypeRechercheTexte.COMPLEXE, motsClefs);
 			
-			case 2:
-				System.out.println("Entrez les mot clefs, en les séparant par une virgules ','");
-				String motsClefs = Clavier.entrerClavierString();
-				resultats = controlRechercher.rechercherTexteMotClef(TypeRechercheTexte.COMPLEXE, motsClefs);
-				break;
-			}
 			break;
 			
 		case 2:
@@ -75,21 +52,6 @@ public class BoundaryRechercherTexte {
 		System.out.println("Résultats de la recherche :");
 		for(int i = 0; i < resultats.size(); i++) {
 			System.out.println((i+1) + ": " + resultats.get(i));
-		}
-		
-	
-		int choixFichier;
-		do {
-			System.out.println("Entrez le numéro du fichier que vous voulez ouvrir?");
-			
-			choixFichier = Clavier.entrerClavierInt();
-			
-			if((choixFichier < 1 || choixFichier > resultats.size())) {
-				System.out.println("Mauvais numéro");
-			}
-		}while ((choixFichier < 1 || choixFichier > resultats.size()));
-		
-		Fichier.ouvrirFichierTexte(resultats.get(choixFichier-1));
-		Fichier.ouvrirFichierTexte(resultats.get(choixFichier-1));
+		}		
 	}	
 }
